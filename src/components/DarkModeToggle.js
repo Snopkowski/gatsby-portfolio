f02aabb47bc4f48801fe7465e3d426e7de6f5227
Snopkowski <wojciechsnopkowski@gmail.com>
2019-10-25 00:13:55 +0100

    darkmode toggle tweaks

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -9,13 +9,7 @@ const DarkModeToggle = () => {
 
   return (
     <div className={ToggleStyles.darkModeToggle}>
-      <button type="button" onClick={darkMode.disable}>
-      &#9728;
-      </button>
       <Toggle checked={darkMode.value} onChange={darkMode.toggle} />
-      <button type="button" onClick={darkMode.enable}>
-      &#9790;
-      </button>
     </div>
   );
 };
