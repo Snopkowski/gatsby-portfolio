87fbd9004baf98867ae0c022d52a053b966adf26
Snopkowski <wojciechsnopkowski@gmail.com>
2019-11-13 15:50:21 +0000

    changed images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,47 @@
 import React from "react"
-import Head from '../components/head'
+import Head from "../components/head"
 import Layout from "../components/layout"
-import indexStyles from '../styles/index.scss'
-import homepageStyles from '../styles/homepage.module.scss'
-import Img from 'gatsby-image'
+import indexStyles from "../styles/index.scss"
+import homepageStyles from "../styles/homepage.module.scss"
+import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
-
-
-
-
+import Hero from "../images/coding.svg"
+import About from "../images/about.svg"
+import Skills from "../images/skills.svg"
 
 const IndexPage = () => {
-
-  const data = useStaticQuery(graphql`
-  query {
-    file(name: {eq: "hero-image"}) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-  }
-  `)
-
   return (
     <div>
       <Layout>
         <Head title="Home" />
         <div className={homepageStyles.container}>
-        <h1 className={indexStyles.mainHeading}>Hello, my name is Wojciech. I'm a Web Developer based in Birmingham, UK.</h1>
-        <Img fluid={data.file.childImageSharp.fluid} />
+          <h1 className={indexStyles.mainHeading}>
+            Hello, my name is Wojciech. I'm a Web Developer based in Birmingham,
+            UK.
+          </h1>
+          <img src={Hero} alt="coding" />
         </div>
         <h3>About</h3>
-        <p>Looking forward to a much deeper dive into coding and programming. I’ve noticed as a self-taught Web Developer, learning about this field has a tremendous momentum to it. I am determined to contribute to this momentum, especially since I am now pursuing my passion to become a professional developer.</p>
-        <p>For the past six years, I’ve been working as an Autoclave Technician. Over time, I’ve come to realize that my true passion is in coding and programming, especially now that the web development industry continues to grow at an exponential rate. Therefore, I’ve decided to transition careers because I trust that my creative skills are likely better suited for the ever-growing web development industry.</p>
-        <p>I am confident that I will continue to absorb new skills and technologies while managing my tasks and building my experience. My ultimate goal is to continually learn something new every day while I fine-tune my abilities now that I have fully committed to immersing myself in this field.</p>
+        <div className={homepageStyles.container}>
+          <img src={About} alt="about" />
+          <p>
+            Looking forward to a much deeper dive into coding and programming.
+            I’ve noticed as a self-taught Web Developer, learning about this
+            field has a tremendous momentum to it. I am determined to contribute
+            to this momentum, especially since I am now pursuing my passion to
+            become a professional developer. My ultimate goal is to continually
+            learn something new every day while I fine-tune my abilities now
+            that I have fully committed to immersing myself in this field.
+          </p>
+        </div>
         <h3>Tools and technologies I've been using lately:</h3>
-        <p>HTML5 - CSS3 - JAVASCRIPT - SASS - STYLED COMPONENTS - FIREBASE - REACT - CSS MODULES - GATSBY - GRAPHQL - GIT - AJAX - NPM - BASH</p>
+        <div className={homepageStyles.container}>
+          <p>
+            HTML5 - CSS3 - JAVASCRIPT - SASS - STYLED COMPONENTS - FIREBASE -
+            REACT - CSS MODULES - GATSBY - GRAPHQL - GIT - AJAX - NPM - BASH
+          </p>
+          <img src={Skills} alt="skills" />
+        </div>
       </Layout>
     </div>
   )
