feaf750dadd5fbb5f34e2b5e10985a836d1941d7
Snopkowski <wojciechsnopkowski@gmail.com>
2019-11-19 14:14:04 +0000

    fixed buttons

--- src/templates/portfolioItem.js
+++ src/templates/portfolioItem.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Img from "gatsby-image"
 import styled from "styled-components"
-import { Button} from '../components/common'
+import { ButtonSmall} from '../components/common'
 
 const PortfolioItem = ({ portfolio }) => {
   const {
@@ -12,11 +12,6 @@ const PortfolioItem = ({ portfolio }) => {
     image,
     description,
   } = portfolio.frontmatter
-
-  const InlineButton = styled(Button)`
-  display: inline-block;
-  margin-right: 2rem;
-  `
 
   const PortfolioItemWrap = styled.div`
     > h3 {
@@ -32,8 +27,8 @@ const PortfolioItem = ({ portfolio }) => {
       <h2>{title}</h2>
       <Img fluid={image.childImageSharp.fluid} />
       <h3>{stack}</h3>
-      <a href={live}><InlineButton>Live</InlineButton></a>
-      <a href={source}><InlineButton>Code</InlineButton></a>
+      <a href={live}><ButtonSmall>Live</ButtonSmall></a>
+      <a href={source}><ButtonSmall>Code</ButtonSmall></a>
       <p>{description}</p>
     </PortfolioItemWrap>
   )
